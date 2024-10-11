bed2043143fd59e2abb39315d24c2ff6b5c13d5d
15'Slamyxx <140884344+15Slamyxx@users.noreply.github.com>
2024-10-12 01:09:03 +0200

    Change Plugin.cs

--- Ultimate-Auto-Cassie/Plugin.cs
+++ Ultimate-Auto-Cassie/Plugin.cs
@@ -1,4 +1,5 @@
 using Exiled.API.Features;
+using System;
 
 namespace Ultimate_Auto_Cassie;
 
@@ -7,14 +8,19 @@ public class Plugin : Plugin<Config>
     public override string Name => "Ultimate-Auto-Cassie";
     public override Version Version => new Version(1, 1, 0);
     public static Plugin Singleton;
+    public static EventHandlers Handlers;
 
     public override void OnEnabled()
     {
+        Exiled.Events.Handlers.Server.RoundStarted += Handlers.OnRoundStarted;
+        Exiled.Events.Handlers.Server.RoundEnded += Handlers.OnRoundEnded;
         base.OnEnabled();
     }
 
     public override void OnDisabled() 
     {
+        Exiled.Events.Handlers.Server.RoundStarted -= Handlers.OnRoundStarted;
+        Exiled.Events.Handlers.Server.RoundEnded -= Handlers.OnRoundEnded;
         base.OnDisabled();
     }
 }
